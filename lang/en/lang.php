99e56fcf58176c4c4d873116218aaa6b364d656f
00MostafaMohsen00 <geye096@gmail.com>
2024-09-13 21:29:26 +0300

    Laravue_13_09_2024

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -6,7 +6,7 @@ return [
     "listeing_deleted" => "Listeing Deleted Successfully . . .",
     "image_uploaded" => "Image Uploaded Successfully . . .",
     "image_deleted" => "Image Deleted Successfully . . .",
-    "marked_as_read" => "Notification marked as read.",
+    "marked_as_read" => "Notification Marked As Read.",
     "offer_created" => "Offer Created Successfully . . .",
     "offer_accepted" => "Offer Accepted Successfully . . .",
     'listeing_restored' => 'Listeing Restored Successfully . . .',
--- resources/js/Components/UI/Message.vue
+++ resources/js/Components/UI/Message.vue
@@ -1,28 +1,24 @@
-<template>
-    <transition>
-        <div
-            v-if="message && showMessage"
-            class="mb-4 border rounded shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2"
-        >
-            {{ message }}
-        </div>
-    </transition>
-</template>
+<template></template>
 
 <script setup>
-import { computed, ref, watch } from "vue";
+import { computed, watch } from "vue";
 import { usePage } from "@inertiajs/vue3";
+import { useToast } from "vue-toast-notification";
 
 const page = usePage();
-const showMessage = ref(false);
 const message = computed(() => {
-    showMessage.value = true;
     return page.props.flash.success;
 });
 
-watch(showMessage, () => {
-    setTimeout(() => {
-        showMessage.value = false;
-    }, 1000);
+watch(message, () => {
+    if (message.value)
+        useToast().open({
+            message: message.value,
+            type: "success",
+            position: "top-right",
+            duration: 3000,
+            dismissible: true,
+            queue: false,
+        });
 });
 </script>
