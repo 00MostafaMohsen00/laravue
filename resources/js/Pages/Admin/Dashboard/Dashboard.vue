2169c1ed926fe121398c3806dcd3c1ccc929936a
00MostafaMohsen00 <geye096@gmail.com>
2025-02-01 13:24:25 +0200

    Laravue_01_02_2025

--- resources/js/Pages/Admin/Dashboard/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard/Dashboard.vue
@@ -1,12 +1,27 @@
 <template>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <box>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <box class="bg-blue-100 dark:bg-blue-900">
             <template #title>
                 <i class="fas fa-users icon"></i> {{ $t("Users") }}
             </template>
             <div class="text-3xl font-bold">{{ users }}</div>
         </box>
-        <box>
+        <box class="bg-green-100 dark:bg-green-900">
+            <template #title>
+                <i class="fas fa-users icon"></i>{{ $t("active") }}
+                {{ $t("Users") }}
+            </template>
+            <div class="text-3xl font-bold">{{ active_users }}</div>
+        </box>
+
+        <box class="bg-red-100 dark:bg-red-900">
+            <template #title>
+                <i class="fas fa-users icon"></i>{{ $t("inactive") }}
+                {{ $t("Users") }}
+            </template>
+            <div class="text-3xl font-bold">{{ inactive_users }}</div>
+        </box>
+        <box class="bg-yellow-100 dark:bg-yellow-900">
             <template #title>
                 <i class="fas fa-home icon"></i>
                 {{ $t("Listeings") }}
@@ -23,5 +38,7 @@ import Box from "@/Components/UI/Box.vue";
 defineProps({
     users: Number,
     listeings: Number,
+    active_users: Number,
+    inactive_users: Number,
 });
 </script>
